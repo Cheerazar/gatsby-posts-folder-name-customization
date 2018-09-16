45e3276cdc887685a22078eac0f96093ca88791f
Gunnari Auvinen <gauvinen@gmail.com>
2018-09-16 11:51:12 -0400

    feature(gatsby-node): Remove MMDDYY-- from beginning of blog post URLs.   - Rename folders to use MMDDYY-- naming structure to get them sorted correctly and such that you don't have to look at index.md to see the date of the post

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,7 +58,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode }).replace(/\d{6}--/, '')
     createNodeField({
       name: `slug`,
       node,
